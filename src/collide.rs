2b2f1a5fe3725adddc1e51cf11d39e84188ca090
Austin Bingham <abingham@Austins-MBP.bingham.lan>
2020-02-09 16:01:31 +0100

    Docs and tests.

--- src/collide.rs
+++ src/collide.rs
@@ -87,63 +87,42 @@ fn solve_quadratic(a: f64, b: f64, c: f64) -> Vec<f64> {
     }
 }
 
+/// Calculate the point at which a projectile would collide with a target.Collidable
+///
+/// * `position` - where the projectile will be launched from
+/// * `speed` - the speed of the projectile
+/// * `target` - the thing we're looking to hit.
+///
+/// The basic calculation is to find a time in the future when the distance of the projectile and the target is the
+/// same. This results in a quadratic equation which we solve. If this gives results, we choose the closest time, figure
+/// out where the target will be at that time, and return that.
 pub fn collision_point(
     position: &Point2<f64>,
     speed: f64,
-    target: &dyn Collidable
+    target: &dyn Collidable,
 ) -> Option<Point2<f64>> {
     let delta_x = position[0] - target.position()[0];
     let delta_y = position[1] - target.position()[1];
 
     let target_speed = target.velocity().speed();
     let target_bearing = target.velocity().bearing();
-    let a = target_speed.powf(2.0) * f64::cos(target_bearing).powf(2.0) + target_speed.powf(2.0) * f64::sin(target_bearing).powf(2.0) - speed.powf(2.0);
-    let b = -1.0 * (2.0 * delta_x * target_speed * f64::cos(target_bearing) + 2.0 * delta_y * target_speed * f64::sin(target_bearing));
+    let cos_target_bearing = f64::cos(target_bearing);
+    let sin_target_bearing = f64::sin(target_bearing);
+    let a = target_speed.powf(2.0) * cos_target_bearing.powf(2.0)
+        + target_speed.powf(2.0) * sin_target_bearing.powf(2.0)
+        - speed.powf(2.0);
+    let b = -1.0
+        * (2.0 * delta_x * target_speed * cos_target_bearing
+            + 2.0 * delta_y * target_speed * sin_target_bearing);
     let c = delta_x.powf(2.0) + delta_y.powf(2.0);
 
-    println!("target_speed {} target_bearing {} a {} b {} c {}", target_speed, target_bearing, a, b, c);
-
     solve_quadratic(a, b, c)
         .iter()
-        .filter(|r| {
-            **r >= 0.0
-        })
-        .min_by(|a, b| {
-            a.partial_cmp(b).unwrap_or(Ordering::Less)
-        })
+        .filter(|r| **r >= 0.0)
+        .min_by(|a, b| a.partial_cmp(b).unwrap_or(Ordering::Less))
         .map(|dt| {
-            println!("dt: {}", dt);
             let coll_x = dt * target_speed * f64::cos(target_bearing) + target.position().x;
             let coll_y = dt * target_speed * f64::sin(target_bearing) + target.position().y;
-            Point2::new(coll_x, coll_y)
-        })
-}
-
-pub fn _collision_point(
-    position: &Point2<f64>,
-    speed: f64,
-    target: &dyn Collidable,
-) -> Option<Point2<f64>> {
-    let dx = target.position().x - position.x;
-    let dy = target.position().y - position.y;
-    let target_speed = target.velocity().speed();
-    let target_dir = target.velocity().bearing();
-    let a = speed.powf(2.0) - target_speed.powf(2.0);
-    let b = -2.0
-        * (target_speed * f64::cos(target_dir) * dx + target_speed * f64::sin(target_dir) * dy);
-    let c = -1.0 * dx.powf(2.0) + dy.powf(2.0);
-
-    solve_quadratic(a, b, c)
-        .iter()
-        .filter(|r| {
-            **r >= 0.0
-        })
-        .min_by(|a, b| {
-            a.partial_cmp(b).unwrap_or(Ordering::Less)
-        })
-        .map(|dt| {
-            let coll_x = dt * target_speed * f64::cos(target_dir) + target.position().x;
-            let coll_y = dt * target_speed * f64::sin(target_dir) + target.position().y;
             Point2::new(coll_x, coll_y)
         })
 }
@@ -151,17 +130,43 @@ pub fn _collision_point(
 /// Calculate vector from `position` to target that will result in
 /// a collision given the other parameters.
 ///
-/// :param position: The firing position.
-/// :param speed: The speed of the projectile.
-/// :param target_pos: Initial position of target to hit.
-/// :param target_speed: Speed of target to hit.
-/// :param target_dir: Direction (of movement) of target
+/// * position - The firing position.
+/// * speed - The speed of the projectile.
+/// * target_pos - Initial position of target to hit.
+/// * target_speed - Speed of target to hit.
+/// * target_dir - Direction (of movement) of target
 ///
-/// :return: A tuple (collision-position, collision-vector) if one
+/// Returns a tuple (collision-position, collision-vector) if one.
 pub fn collision_vector(
     position: &Point2<f64>,
     speed: f64,
     target: &dyn Collidable,
 ) -> Option<(Point2<f64>, Vector2<f64>)> {
     collision_point(position, speed, target).map(|p| (p, p - position))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod quadratic {
+        use super::*;
+
+        #[test]
+        fn test_canned() {
+            let cases: Vec<((f64, f64, f64), Vec<f64>)> = vec![
+                ((1.0, 9.0, 14.0), vec![-2.0, -7.0]),
+                ((1.0, -9.0, 14.0), vec![7.0, 2.0]),
+                ((1.0, 2.0, -15.0), vec![3.0, -5.0]),
+                ((1.0, -2.0, -15.0), vec![5.0, -3.0]),
+                ((2.0, 15.0, 27.0), vec![-3.0, -9.0 / 2.0]),
+                ((2.0, -15.0, 27.0), vec![9.0 / 2.0, 3.0]),
+                ((2.0, 3.0, -27.0), vec![3.0, -9.0 / 2.0]),
+            ];
+
+            for ((a, b, c), expected) in cases {
+                assert_eq!(solve_quadratic(a, b, c), expected);
+            }
+        }
+    }
 }
