d7d33fc0f5fd2528399b081dacbee9192512c571
Austin Bingham <austin.bingham@gmail.com>
2020-02-18 15:46:11 +0100

    Experimenting with spec

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 #[macro_use] #[cfg(test)] extern crate approx;
 
-pub mod collide;
-pub mod controller;
-pub mod model;
-pub mod velocity;
-pub mod view;
+// pub mod collide;
+// pub mod controller;
+// pub mod model;
+// pub mod velocity;
+// pub mod view;
+
+// https://specs.amethyst.rs/docs/tutorials/02_hello_world.html
