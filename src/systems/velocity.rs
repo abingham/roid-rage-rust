aa1fced5bbee9bd39b3d00db4d02961d911bbdd4
Austin Bingham <abingham@Austins-MBP.bingham.lan>
2020-02-29 15:04:31 +0100

    Oops.

--- src/systems/velocity.rs
+++ src/systems/velocity.rs
@@ -1,36 +1,18 @@
-use crate::components::{Velocity, Wrapping};
-use crate::field::Field;
+use crate::components::Velocity;
 use amethyst::core::transform::Transform;
-use amethyst::ecs::{Join, ReadExpect, ReadStorage, System, WriteStorage};
+use amethyst::ecs::{Join, ReadStorage, System, WriteStorage};
 
 pub struct VelocitySystem;
 
 impl<'s> System<'s> for VelocitySystem {
-    type SystemData = (
-        WriteStorage<'s, Transform>,
-        ReadStorage<'s, Velocity>,
-        ReadStorage<'s, Wrapping>,
-        ReadExpect<'s, Field>,
-    );
+    type SystemData = (WriteStorage<'s, Transform>, ReadStorage<'s, Velocity>);
 
-    fn run(&mut self, (mut transforms, velocities, wrapping, field): Self::SystemData) {
+    fn run(&mut self, (mut transforms, velocities): Self::SystemData) {
         // TODO: Distance should be based on time-delta.
 
-        // Processing entities that wrap
-        for (velocity, transform, _) in (&velocities, &mut transforms, &wrapping).join() {
-            let (new_x, new_y) = field.wrap(
-                transform.translation().x + velocity.vec.x,
-                transform.translation().y + velocity.vec.y,
-            );
-
-            transform.set_translation_y(new_y);
-            transform.set_translation_x(new_x);
-        }
-
         // Processing entities that don't wrap
-        for (velocity, transform, _) in (&velocities, &mut transforms, !&wrapping).join() {
+        for (velocity, transform) in (&velocities, &mut transforms).join() {
             transform.set_translation_x(transform.translation().x + velocity.vec.x);
-
             transform.set_translation_y(transform.translation().y + velocity.vec.y);
         }
 
