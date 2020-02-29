c260e8380f9c08e7870d1a0c09ab3cca63f4113f
Austin Bingham <abingham@Austins-MBP.bingham.lan>
2020-02-29 15:23:51 +0100

    Using time-delta for velocity updates.

--- src/systems/velocity.rs
+++ src/systems/velocity.rs
@@ -1,21 +1,26 @@
 use crate::components::Velocity;
+use amethyst::core::timing::Time;
 use amethyst::core::transform::Transform;
-use amethyst::ecs::{Join, ReadStorage, System, WriteStorage};
+use amethyst::ecs::{Join, Read, ReadStorage, System, WriteStorage};
 
 pub struct VelocitySystem;
 
 impl<'s> System<'s> for VelocitySystem {
-    type SystemData = (WriteStorage<'s, Transform>, ReadStorage<'s, Velocity>);
+    type SystemData = (
+        WriteStorage<'s, Transform>,
+        ReadStorage<'s, Velocity>,
+        Read<'s, Time>,
+    );
 
-    fn run(&mut self, (mut transforms, velocities): Self::SystemData) {
-        // TODO: Distance should be based on time-delta.
-
-        // Processing entities that don't wrap
+    fn run(&mut self, (mut transforms, velocities, time): Self::SystemData) {
+        // Move all of the moving objects
         for (velocity, transform) in (&velocities, &mut transforms).join() {
-            transform.set_translation_x(transform.translation().x + velocity.vec.x);
-            transform.set_translation_y(transform.translation().y + velocity.vec.y);
+            transform.set_translation_x(
+                transform.translation().x + velocity.vec.x * time.delta_seconds(),
+            );
+            transform.set_translation_y(
+                transform.translation().y + velocity.vec.y * time.delta_seconds(),
+            );
         }
-
-        // TODO: Create new system to remove things that are off the field
     }
 }
