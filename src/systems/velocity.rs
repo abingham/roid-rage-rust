8b6d5af2f8b08086de3deaa52ba085ac1b46efa5
Austin Bingham <abingham@Austins-MBP.bingham.lan>
2020-02-29 15:01:44 +0100

    Wrapping objects that wrap.

--- src/systems/velocity.rs
+++ src/systems/velocity.rs
@@ -1,22 +1,39 @@
-use crate::components::Velocity;
+use crate::components::{Velocity, Wrapping};
+use crate::field::Field;
 use amethyst::core::transform::Transform;
-use amethyst::ecs::{Join, ReadStorage, System, WriteStorage};
+use amethyst::ecs::{Join, ReadExpect, ReadStorage, System, WriteStorage};
 
 pub struct VelocitySystem;
 
 impl<'s> System<'s> for VelocitySystem {
-    type SystemData = (WriteStorage<'s, Transform>, ReadStorage<'s, Velocity>);
+    type SystemData = (
+        WriteStorage<'s, Transform>,
+        ReadStorage<'s, Velocity>,
+        ReadStorage<'s, Wrapping>,
+        ReadExpect<'s, Field>,
+    );
 
-    fn run(&mut self, (mut transforms, velocities): Self::SystemData) {
-        for (velocity, transform) in (&velocities, &mut transforms).join() {
-            // TODO: Need to distinguish between objects that wrap and those which exit the field. A marker component with null storage.
-            // TODO: Distance should be based on time-delta.
+    fn run(&mut self, (mut transforms, velocities, wrapping, field): Self::SystemData) {
+        // TODO: Distance should be based on time-delta.
 
-            let start_y = transform.translation().y;
-            transform.set_translation_y(start_y + velocity.vec.y);
+        // Processing entities that wrap
+        for (velocity, transform, _) in (&velocities, &mut transforms, &wrapping).join() {
+            let (new_x, new_y) = field.wrap(
+                transform.translation().x + velocity.vec.x,
+                transform.translation().y + velocity.vec.y,
+            );
 
-            let start_x = transform.translation().x;
-            transform.set_translation_x(start_x + velocity.vec.x);
+            transform.set_translation_y(new_y);
+            transform.set_translation_x(new_x);
         }
+
+        // Processing entities that don't wrap
+        for (velocity, transform, _) in (&velocities, &mut transforms, !&wrapping).join() {
+            transform.set_translation_x(transform.translation().x + velocity.vec.x);
+
+            transform.set_translation_y(transform.translation().y + velocity.vec.y);
+        }
+
+        // TODO: Create new system to remove things that are off the field
     }
 }
