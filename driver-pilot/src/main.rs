958f27e7700daeb27f87802f399f8680443f6c5f
Austin Bingham <austin.bingham@gmail.com>
2020-04-11 13:06:55 +0200

    Added experimental driver-pilot.
    This is a sandbox for playing with driving the ship around.

---
+++ driver-pilot/src/main.rs
@@ -0,0 +1,37 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+extern crate nalgebra;
+
+use rocket::{catch, catchers, post, routes};
+use rocket_contrib::json;
+use rocket_contrib::json::{Json, JsonValue};
+use roid_rage::core::pilot::{Command, GameState};
+use roid_rage::core::velocity::Velocity;
+
+#[post("/", format = "json", data = "<game_state>")]
+fn update(game_state: Json<GameState>) -> Json<Command> {
+    let cmd = Command {
+        fire: false,
+        rotation: 0,
+    };
+
+    Json(cmd)
+}
+
+#[catch(404)]
+fn not_found() -> JsonValue {
+    json!({
+        "status": "error",
+        "reason": "Resource was not found."
+    })
+}
+
+fn rocket() -> rocket::Rocket {
+    rocket::ignite()
+        .mount("/", routes![update])
+        .register(catchers![not_found])
+}
+
+fn main() {
+    rocket().launch();
+}
