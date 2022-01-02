939842bd392e1be1005cc266ea8fd2a495a455fa
Austin Bingham <austin@sixty-north.com>
2022-01-02 19:39:23 +0100

    Prepared ship spawning for dynamic pilots.

--- roid-rage/src/systems/respawn_ship.rs
+++ roid-rage/src/systems/respawn_ship.rs
@@ -1,7 +1,9 @@
 /// System responsible for creating ships for pilots with no ships.
 use crate::components::{make_ship, Cannon, Pilot, Ship};
 use crate::settings::Settings;
-use specs::{Builder, Entities, LazyUpdate, Read, ReadExpect, ReadStorage, System, WriteExpect};
+use specs::{
+    Builder, EntityBuilder, Entities, Join, LazyUpdate, Read, ReadExpect, ReadStorage, System, WriteExpect, WriteStorage
+};
 use sted::Bearing;
 
 /// Respawn the ship if needed
@@ -9,46 +11,54 @@ pub struct RespawnShipSystem;
 
 impl<'s> System<'s> for RespawnShipSystem {
     type SystemData = (
+        WriteStorage<'s, Pilot>,
         ReadStorage<'s, Ship>,
         Entities<'s>,
         ReadExpect<'s, Settings>,
         Read<'s, LazyUpdate>,
     );
 
-    fn run(&mut self, (ships, entities, settings, lazy): Self::SystemData) {
-        if !ships.is_empty() {
-            return;
+    fn run(&mut self, (mut pilots, ships, entities, settings, lazy): Self::SystemData) {
+        // TEMPORARY: If there are no pilots, create one without a ship.
+        // TODO: Eventually this will happen as a result of a message from a new pilot process.
+        if pilots.is_empty() {
+            let new_entity = entities.create();
+            match pilots.insert(new_entity, Pilot::new()) {
+                Err(_) => println!("oops! Trouble creating pilot"),
+                Ok(_) => println!("new pilot")
+            }
         }
+        
+        // Find all pilots without a ship.
+        for (_pilot, entity, ()) in (&pilots, &entities, !&ships).join() {
+            let position_x = settings.screen_width / 2.0;
+            let position_y = settings.screen_height / 2.0;
+            let speed = 0.0;
+            let course = Bearing::new(0.0);
+            let heading = Bearing::new(0.0);
 
-        let position_x = settings.screen_width / 2.0;
-        let position_y = settings.screen_height / 2.0;
-        let speed = 0.0;
-        let course = Bearing::new(0.0);
-        let heading = Bearing::new(0.0);
-        let new_entity = entities.create();
-
-        // Arrange for this ship to be created.
-        make_ship(
-            specs::world::LazyBuilder {
-                entity: new_entity,
-                lazy: &*lazy,
-            },
-            heading,
-            settings.ship_length,
-            settings.ship_width,
-            settings.ship_mass,
-            settings.ship_thrust,
-            settings.ship_rotational_speed,
-            Cannon {
-                bullet_speed: settings.bullet_speed,
-                rate_of_fire: settings.rate_of_fire,
-            },
-            position_x,
-            position_y,
-            speed,
-            course,
-        )
-        .with(Pilot::new())
-        .build();
+            // Arrange for a ship to be created.
+            make_ship(
+                specs::world::LazyBuilder {
+                    entity: entity,
+                    lazy: &*lazy,
+                },
+                heading,
+                settings.ship_length,
+                settings.ship_width,
+                settings.ship_mass,
+                settings.ship_thrust,
+                settings.ship_rotational_speed,
+                Cannon {
+                    bullet_speed: settings.bullet_speed,
+                    rate_of_fire: settings.rate_of_fire,
+                },
+                position_x,
+                position_y,
+                speed,
+                course,
+            )
+            .build();
+        }
     }
 }
