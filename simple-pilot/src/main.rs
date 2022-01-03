581f77b141dba5db0bcb36b2df37611182fa72f3
Austin Bingham <austin@sixty-north.com>
2022-01-03 23:41:50 +0100

    Using pilot_main in simple-pilot.

--- simple-pilot/src/main.rs
+++ simple-pilot/src/main.rs
@@ -5,30 +5,22 @@
 extern crate nalgebra;
 
 // use crate::targeting::find_target;
-use tonic::{transport::Server, Request, Response, Status};
+use tonic::{Request, Response, Status};
 
-use roid_rage_grpc::roid_rage::pilot_server::{
-    Pilot, PilotServer
-};
+use roid_rage_grpc::roid_rage::pilot_server::Pilot;
 
-use roid_rage_grpc::roid_rage::{
-  Command, GameState, Rotation
-};
+use roid_rage_grpc::roid_rage::{Command, GameState, Rotation};
 
 #[derive(Default)]
 struct SimplePilot {}
 
 #[tonic::async_trait]
 impl Pilot for SimplePilot {
-    async fn get_command(
-        &self,
-        _request: Request<GameState>,
-    ) -> Result<Response<Command>, Status>
-    {
+    async fn get_command(&self, _request: Request<GameState>) -> Result<Response<Command>, Status> {
         let command = Command {
             fire: false,
             rotation: Rotation::None as i32,
-            thrusters: false
+            thrusters: false,
         };
         Ok(Response::new(command))
     }
@@ -36,19 +28,6 @@ impl Pilot for SimplePilot {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // dotenv().ok();
-    // env_logger::init();
-
-    // info!("Starting simple-pilot");
-
-    let addr = "[::1]:50051".parse().unwrap();
-    // let addr = std::env::var("GRPC_SERVER_ADDRESS")?.parse()?;
-
     let pilot = SimplePilot::default();
-    let svc = PilotServer::new(pilot);
-
-    Server::builder().add_service(svc).serve(addr).await?;
-
-    Ok(())
+    roid_rage::pilot_base::pilot_main(pilot).await
 }
-
