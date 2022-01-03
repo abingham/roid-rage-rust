6412f5428d84cba6589102fbaefaf318a6858bf8
Austin Bingham <austin@sixty-north.com>
2022-01-03 23:02:34 +0100

    Refactored manual-pilot a bit.

--- manual-pilot/src/main.rs
+++ manual-pilot/src/main.rs
@@ -1,53 +1,7 @@
-use device_query::{DeviceQuery, DeviceState, Keycode};
 use roid_rage_grpc::roid_rage as rpc;
-use roid_rage_grpc::roid_rage::pilot_server::{Pilot, PilotServer};
+use roid_rage_grpc::roid_rage::pilot_server::PilotServer;
 use structopt::StructOpt;
-use tonic::{transport::Server, Request, Response, Status};
-
-struct PilotState {
-    device_state: DeviceState,
-}
-
-impl PilotState {
-    fn new() -> PilotState {
-        PilotState {
-            device_state: DeviceState::new(),
-        }
-    }
-}
-
-#[tonic::async_trait]
-impl Pilot for PilotState {
-    async fn get_command(
-        &self,
-        _request: Request<rpc::GameState>,
-    ) -> Result<Response<rpc::Command>, Status> {
-        let mut cmd = rpc::Command::null();
-
-        let keys: Vec<Keycode> = self.device_state.get_keys();
-
-        for key in keys {
-            cmd = match key {
-                Keycode::Right => rpc::Command {
-                    rotation: rpc::Rotation::Clockwise as i32,
-                    ..cmd
-                },
-                Keycode::Left => rpc::Command {
-                    rotation: rpc::Rotation::Counterclockwise as i32,
-                    ..cmd
-                },
-                Keycode::Up => rpc::Command {
-                    thrusters: true,
-                    ..cmd
-                },
-                Keycode::Space => rpc::Command { fire: true, ..cmd },
-                _ => cmd,
-            }
-        }
-
-        Ok(Response::new(cmd))
-    }
-}
+use tonic::transport::Server;
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "basic")]
@@ -56,8 +10,8 @@ struct Opt {
     pilot_address: String,
 
     // An argument of type float, with a default value.
-    #[structopt(short, long, default_value = "[::1]:50051", name="address")]
-    server: String
+    #[structopt(short, long, default_value = "[::1]:50051", name = "address")]
+    server: String,
 }
 
 #[tokio::main]
@@ -68,7 +22,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let pilot_address = opt.pilot_address;
     let pilot_url = format!("http://{}", pilot_address);
 
-    let pilot = PilotState::new();
+    let pilot = manual_pilot::pilot::PilotState::new();
     let svc = PilotServer::new(pilot);
 
     // Run the pilot service
@@ -78,7 +32,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             .serve(pilot_address.parse().unwrap()),
     );
 
-    // TODO: The address of the server should be a command line option or something.
     let server_address = format!("http://{}", opt.server);
 
     // Register with game
---
+++ manual-pilot/src/pilot.rs
@@ -0,0 +1,50 @@
+use device_query::{DeviceQuery, DeviceState, Keycode};
+use roid_rage_grpc::roid_rage as rpc;
+use roid_rage_grpc::roid_rage::pilot_server::{Pilot};
+use tonic::{Request, Response, Status};
+
+pub struct PilotState {
+    device_state: DeviceState,
+}
+
+impl PilotState {
+    pub fn new() -> PilotState {
+        PilotState {
+            device_state: DeviceState::new(),
+        }
+    }
+}
+
+#[tonic::async_trait]
+impl Pilot for PilotState {
+    async fn get_command(
+        &self,
+        _request: Request<rpc::GameState>,
+    ) -> Result<Response<rpc::Command>, Status> {
+        let mut cmd = rpc::Command::null();
+
+        let keys: Vec<Keycode> = self.device_state.get_keys();
+
+        for key in keys {
+            cmd = match key {
+                Keycode::Right => rpc::Command {
+                    rotation: rpc::Rotation::Clockwise as i32,
+                    ..cmd
+                },
+                Keycode::Left => rpc::Command {
+                    rotation: rpc::Rotation::Counterclockwise as i32,
+                    ..cmd
+                },
+                Keycode::Up => rpc::Command {
+                    thrusters: true,
+                    ..cmd
+                },
+                Keycode::Space => rpc::Command { fire: true, ..cmd },
+                _ => cmd,
+            }
+        }
+
+        Ok(Response::new(cmd))
+    }
+}
+
