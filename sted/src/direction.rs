5fe8c03686959ea17c15c9a846059f299fb05d2a
Austin Bingham <austin@sixty-north.com>
2022-01-08 15:39:04 +0100

    Fixed Diection.vector().

--- sted/src/direction.rs
+++ sted/src/direction.rs
@@ -9,30 +9,6 @@ pub trait Direction {
         let y = self.radians().sin();
         glam::Vec2::new(x, y)
     }
-
-    // Get the bearing of the direction in radians.
-    //
-    // CCW azimuthal direction.
-    // fn bearing(&self) -> f32 {
-    //     let x = self.dx();
-    //     // NB: We flip the y axis because our coord. system is
-    //     // different from the math one. I.e. for us -y is "up".
-    //     let y = -1.0 * self.dy();
-    //     f32::atan2(y, x)
-    // }
-
-    // fn rotate(&self, amount: f32) -> Self where Self: Sized {
-    //     // TODO: This is nuts. Should we just hardcode things to f32?
-
-    //     let theta = self.bearing() + amount;
-
-    //     let len = (self.dx().powf(2.0) + self.dy().powf(2.0)).sqrt();
-    //     let new_x = theta.cos() * len;
-    //     let new_y = theta.sin() * len;
-    //     let new = Self::create(new_x, 1.0 * new_y);
-    //     println!("{:?} {:?}", new_x, new_y);
-    //     new
-    // }
 }
 
 impl Direction for f32 {
@@ -70,7 +46,8 @@ mod tests {
     fn north() {
         let north = PI / 2.0;
         let actual = north.vector();
-        let expected = glam::Vec2::new(0.0, -1.0);
+        let expected = glam::Vec2::new(0.0, 1.0);
+        // assert_eq!(actual, expected);
         assert!((actual.x - expected.x).abs() < 0.0001);
         assert!((actual.y - expected.y).abs() < 0.0001);
     }
@@ -79,7 +56,7 @@ mod tests {
     fn south() {
         let south = 3.0 * PI / 2.0;
         let actual = south.vector();
-        let expected = glam::Vec2::new(0.0, 1.0);
+        let expected = glam::Vec2::new(0.0, -1.0);
         assert!((actual.x - expected.x).abs() < 0.0001);
         assert!((actual.y - expected.y).abs() < 0.0001);
     }
